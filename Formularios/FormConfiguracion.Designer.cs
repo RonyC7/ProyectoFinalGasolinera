0e9943fa68d7cffd570ee5fb5bcbcdf9b958909f
RonyC7 <carpioalvaradorony@mesoamericana.edu.gt>
2024-04-27 22:00:41 -0600

    Formulario de configuracion del precio de la gasolina por galon terminado, falta ahora form estadistica

--- Formularios/FormConfiguracion.Designer.cs
+++ Formularios/FormConfiguracion.Designer.cs
@@ -29,20 +29,143 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormConfiguracion));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBoxSuper = new System.Windows.Forms.PictureBox();
+            this.pictureBoxDiesel = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRegular = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnGuardarPrecios = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSuper)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDiesel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRegular)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(24, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(319, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Precios de la gasolina por galon";
+            // 
+            // pictureBoxSuper
+            // 
+            this.pictureBoxSuper.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxSuper.Image")));
+            this.pictureBoxSuper.Location = new System.Drawing.Point(27, 55);
+            this.pictureBoxSuper.Name = "pictureBoxSuper";
+            this.pictureBoxSuper.Size = new System.Drawing.Size(84, 40);
+            this.pictureBoxSuper.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxSuper.TabIndex = 1;
+            this.pictureBoxSuper.TabStop = false;
+            // 
+            // pictureBoxDiesel
+            // 
+            this.pictureBoxDiesel.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxDiesel.Image")));
+            this.pictureBoxDiesel.Location = new System.Drawing.Point(143, 55);
+            this.pictureBoxDiesel.Name = "pictureBoxDiesel";
+            this.pictureBoxDiesel.Size = new System.Drawing.Size(84, 40);
+            this.pictureBoxDiesel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxDiesel.TabIndex = 2;
+            this.pictureBoxDiesel.TabStop = false;
+            // 
+            // pictureBoxRegular
+            // 
+            this.pictureBoxRegular.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxRegular.Image")));
+            this.pictureBoxRegular.Location = new System.Drawing.Point(263, 55);
+            this.pictureBoxRegular.Name = "pictureBoxRegular";
+            this.pictureBoxRegular.Size = new System.Drawing.Size(84, 40);
+            this.pictureBoxRegular.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxRegular.TabIndex = 3;
+            this.pictureBoxRegular.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 111);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(84, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(143, 111);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(84, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(263, 111);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(84, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // btnGuardarPrecios
+            // 
+            this.btnGuardarPrecios.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnGuardarPrecios.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.btnGuardarPrecios.FlatAppearance.BorderSize = 2;
+            this.btnGuardarPrecios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardarPrecios.ForeColor = System.Drawing.Color.White;
+            this.btnGuardarPrecios.Location = new System.Drawing.Point(134, 137);
+            this.btnGuardarPrecios.Name = "btnGuardarPrecios";
+            this.btnGuardarPrecios.Size = new System.Drawing.Size(103, 26);
+            this.btnGuardarPrecios.TabIndex = 7;
+            this.btnGuardarPrecios.Text = "Guardar Precios";
+            this.btnGuardarPrecios.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(362, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(331, 211);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
             // 
             // FormConfiguracion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(708, 240);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnGuardarPrecios);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBoxRegular);
+            this.Controls.Add(this.pictureBoxDiesel);
+            this.Controls.Add(this.pictureBoxSuper);
+            this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormConfiguracion";
             this.Text = "Formulario Configuracion";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxSuper)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDiesel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRegular)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBoxSuper;
+        private System.Windows.Forms.PictureBox pictureBoxDiesel;
+        private System.Windows.Forms.PictureBox pictureBoxRegular;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btnGuardarPrecios;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
